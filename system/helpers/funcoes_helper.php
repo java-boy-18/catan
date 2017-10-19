d753fe129cedb0a2d0bf2fed4ef84dd85110e117
java-boy-18 <tribeiromiranda90@gmail.com>
2017-10-19 11:08:25 -0200

    Adicionando arquivo de funcoes

---
+++ system/helpers/funcoes_helper.php
@@ -0,0 +1,27 @@
+<?php
+function extenso($numero){
+  if($numero == 2){
+    $extenso = 'dois';
+  }elseif($numero == 3){
+    $extenso = 'tres';
+  }elseif($numero == 4){
+    $extenso = 'quatro';
+  }elseif($numero == 5){
+    $extenso = 'cinco';
+  }elseif($numero == 6){
+    $extenso = 'seis';
+  }elseif($numero == 7){
+    $extenso = 'sete';
+  }elseif($numero == 8){
+    $extenso = 'oito';
+  }elseif($numero == 9){
+    $extenso = 'nove';
+  }elseif($numero == 10){
+    $extenso = 'dez';
+  }elseif($numero == 11){
+    $extenso = 'onze';
+  }elseif($numero == 12){
+    $extenso = 'doze';
+  }
+  return $extenso;
+}
