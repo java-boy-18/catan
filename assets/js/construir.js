4c1f3fa9fe74de169fd1b3f40be3f01196015299
java-boy-18 <tribeiromiranda90@gmail.com>
2017-11-14 09:58:05 -0200

    Agora a construção verifica se o usuário tem materiais para construir

--- assets/js/construir.js
+++ assets/js/construir.js
@@ -1,13 +1,31 @@
-function construir(esquerda,selecionado,direita,cima =null){
+function construirsettle(esquerda,selecionado,direita,cima =null){
     var DivEsquerda = '#'+esquerda;
     var DivSelecionado = '#'+selecionado;
     var DivDireita = '#'+direita;
     var DivCima = '#'+cima;
-    if($(DivEsquerda).hasClass('addsettlement')||$(DivSelecionado).hasClass('addsettlement')||$(DivDireita).hasClass('addsettlement')||$(DivCima).hasClass('addsettlement')){
-        alert('nao pode construir aqui');
+    var Madeira = $('#inputmadeira').val();
+    var Ovelha = $('#inputovelha').val();
+    var Tijolo = $('#inputtijolo').val();
+    var Trigo = $('#inputtrigo').val();
+    if(Madeira >= 1 && Ovelha >= 1 && Tijolo >= 1 && Trigo>= 1){
+        if($(DivEsquerda).hasClass('addsettlement')||$(DivSelecionado).hasClass('addsettlement')||$(DivDireita).hasClass('addsettlement')||$(DivCima).hasClass('addsettlement')){
+            alert('nao pode construir aqui');
+        }else{
+            $(DivSelecionado).addClass('addsettlement');
+            Madeira--;
+            Ovelha--;
+            Tijolo--;
+            Trigo--;
+            $('#inputmadeira').val(Madeira);
+            $('#inputovelha').val(Ovelha);
+            $('#inputtijolo').val(Tijolo);
+            $('#inputtrigo').val(Trigo);
+        }
     }else{
-        $(DivSelecionado).addClass('addsettlement');
+        alert('Material insuficiente!');
     }
 }
+
 $(document).ready(function () {
+
 });
